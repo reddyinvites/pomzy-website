96c0cd8217e35271379f7aef1de1dbe3edd3a480
reddyinvites <reddyinvites@gmail.com>
2026-04-08 08:52:37 +0530

    Create apppzy.py

---
+++ apppzy.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.title("Pomzy Connect")
+
+st.write("We help PG owners fill vacant beds fast.")
+
+st.header("Contact")
+st.write("📞 7702656073")
+st.write("💬 WhatsApp: 7702656073")
